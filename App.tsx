1e74782c1eb135d75558b8175a5c22a2e794550c
arqgasp <gabrielsilva.arq@gmail.com>
2022-07-21 22:53:49 -0300

    more accomplishment

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/ro
 
 import {THEME} from './src/styles/theme';
 
-import {SignIn} from './src/screens/SignIn' 
+import { Home } from './src/screens/Home' 
 import { Loading } from './src/components/Loading';
 
 
@@ -19,7 +19,7 @@ export default function App() {
       backgroundColor="transparent"
 translucent
       />
-      {fontsLoaded ? <SignIn/> : <Loading/>}
+      {fontsLoaded ? <Home/> : <Loading/>}
     </NativeBaseProvider>
   );
 }
