484f35e129c8a046594a090334332e1e51c1fc37
arqgasp <gabrielsilva.arq@gmail.com>
2022-07-22 22:59:03 -0300

    Almost finish

--- App.tsx
+++ App.tsx
@@ -1,25 +1,26 @@
 import { NativeBaseProvider, StatusBar } from "native-base";
-import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import {THEME} from './src/styles/theme';
+import { THEME } from "./src/styles/theme";
 
-import { Home } from './src/screens/Home' 
-import { Loading } from './src/components/Loading';
-
-
+import { Routes } from "./src/routes";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
-  const [fontsLoaded] = useFonts ({Roboto_400Regular, Roboto_700Bold});
-
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
-      barStyle="light-content"
-      backgroundColor="transparent"
-translucent
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
       />
-      {fontsLoaded ? <Home/> : <Loading/>}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
