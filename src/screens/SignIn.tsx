53e8396f4d9e844153b371ea91554e17ff65ef88
arqgasp <gabrielsilva.arq@gmail.com>
2022-07-23 21:56:29 -0300

    projeto finalizado

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,56 +1,53 @@
-import { useState } from 'react';
-import { VStack, Heading, Icon, useTheme } from 'native-base';
-import {Envelope, Key} from 'phosphor-react-native';
+import { useState } from "react";
+import { Alert } from "react-native";
+import auth from "@react-native-firebase/auth";
+import { VStack, Heading, Icon, useTheme } from "native-base";
+import { Envelope, Key } from "phosphor-react-native";
 
-import Logo from '../assets/logo_primary.svg';
+import Logo from "../assets/logo_primary.svg";
 
-import { Input } from '../components/input';
-import { Button } from '../components/Button';
+import { Input } from "../components/input";
+import { Button } from "../components/Button";
+import React from "react";
 
-
-
-
-export function SignIn(){
-  const [name, setName] = useState('');
-  const [ password, setPassword] = useState('');
-
+export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { colors } = useTheme();
 
   function handleSignIn() {
-    console.log(name, password);
+    if (!email || !password) {
+      return Alert.alert("Entrar", "Informe e-mail e senha.");
+    }
   }
 
-
-
   return (
-<VStack flex={1} alignItems="center"bg="gray.600" px={8} pt={24}>
-<Logo/>
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
 
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse a sua conta
+      </Heading>
 
-<Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
-  Acesse a sua conta
-</Heading>
+      <Input
+        mb={4}
+        placeholder="E-mail"
+        InputLeftElement={
+          <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
+        }
+        onChangeText={setEmail}
+      />
 
+      <Input
+        mb={8}
+        placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+        onChangeText={setPassword}
+      />
 
-<Input 
-mb={4}
-placeholder="E-mail" 
-InputLeftElement={ <Icon as={<Envelope color={colors.gray[300]}  />} ml={4}/> }
-onChangeText= {setName}
-/>
-
-<Input
-mb={8}
-placeholder="Senha" 
-InputLeftElement={ <Icon as={<Key color={colors.gray[300]}  />} ml={4}/> }
-secureTextEntry
-onChangeText={setPassword}
-/>
-
-<Button title="Entrar" w="full" onPress={handleSignIn}/>
-
-
-</VStack>
-  )
+      <Button title="Entrar" w="full" onPress={handleSignIn} />
+    </VStack>
+  );
 }
